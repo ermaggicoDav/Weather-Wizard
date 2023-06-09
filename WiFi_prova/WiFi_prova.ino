800538fcfb89d5f653e8f35b2bc1e1604e30b760
ermaggicoDav <117778201+ermaggicoDav@users.noreply.github.com>
2023-06-09 11:52:34 +0200

    added code for first WiFi try

---
+++ WiFi_prova/WiFi_prova.ino
@@ -0,0 +1,177 @@
+#include <AirQualityClass.h>
+#include <Arduino_MKRIoTCarrier.h>
+#include <Arduino_MKRIoTCarrier_Buzzer.h>
+#include <Arduino_MKRIoTCarrier_Qtouch.h>
+#include <Arduino_MKRIoTCarrier_Relay.h>
+#include <EnvClass.h>
+#include <IMUClass.h>
+#include <MKRIoTCarrierDefines.h>
+#include <PressureClass.h>
+
+MKRIoTCarrier carrier;
+
+
+#include <WiFiNINA.h>
+
+char ssid[] = "f-factory";             //  your network SSID (name) between the " "
+char pass[] = "Z89sn30Pfuq+s9";      // your network password between the " "
+int keyIndex = 0;                 // your network key Index number (needed only for WEP)
+int status = WL_IDLE_STATUS;      //connection status
+WiFiServer server(80);            //server socket
+
+WiFiClient client = server.available();
+
+
+void setup() {
+  Serial.begin(9600);
+  carrier.begin();
+
+  while (!Serial);
+  
+  enable_WiFi();
+  connect_WiFi();
+
+  server.begin();
+  printWifiStatus();
+
+}
+
+void loop() {
+  client = server.available();
+
+  if (client) {
+    printWEB();
+  }
+}
+
+void printWifiStatus() {
+  // print the SSID of the network you're attached to:
+  Serial.print("SSID: ");
+  Serial.println(WiFi.SSID());
+
+  // print your board's IP address:
+  IPAddress ip = WiFi.localIP();
+  Serial.print("IP Address: ");
+  Serial.println(ip);
+
+  // print the received signal strength:
+  long rssi = WiFi.RSSI();
+  Serial.print("signal strength (RSSI):");
+  Serial.print(rssi);
+  Serial.println(" dBm");
+
+  Serial.print("To see this page in action, open a browser to http://");
+  Serial.println(ip);
+}
+
+void enable_WiFi() {
+  // check for the WiFi module:
+  if (WiFi.status() == WL_NO_MODULE) {
+    Serial.println("Communication with WiFi module failed!");
+    // don't continue
+    while (true);
+  }
+
+  String fv = WiFi.firmwareVersion();
+  if (fv < "1.0.0") {
+    Serial.println("Please upgrade the firmware");
+  }
+}
+
+void connect_WiFi() {
+  // attempt to connect to Wifi network:
+  while (status != WL_CONNECTED) {
+    Serial.print("Attempting to connect to SSID: ");
+    Serial.println(ssid);
+    // Connect to WPA/WPA2 network. Change this line if using open or WEP network:
+    status = WiFi.begin(ssid, pass);
+
+    // wait 10 seconds for connection:
+    delay(10000);
+  }
+}
+
+void printWEB() {
+
+  if (client) {                             // if you get a client,
+    Serial.println("new client");           // print a message out the serial port
+    String currentLine = "";                // make a String to hold incoming data from the client
+    while (client.connected()) {            // loop while the client's connected
+      if (client.available()) {             // if there's bytes to read from the client,
+        char c = client.read();             // read a byte, then
+        Serial.write(c);                    // print it out the serial monitor
+        if (c == '\n') {                    // if the byte is a newline character
+
+          // if the current line is blank, you got two newline characters in a row.
+          // that's the end of the client HTTP request, so send a response:
+          if (currentLine.length() == 0) {
+
+            // HTTP headers always start with a response code (e.g. HTTP/1.1 200 OK)
+            // and a content-type so the client knows what's coming, then a blank line:
+            client.println("HTTP/1.1 200 OK");
+            client.println("Content-type:text/html");
+            client.println();
+           
+            //create the buttons
+            client.print("Click <a href=\"/H\">here</a> turn the LED on<br>");
+            client.print("Click <a href=\"/L\">here</a> turn the LED off<br><br>");
+            
+            int randomReading = analogRead(A1);
+            client.print("Random reading from analog pin: ");
+            client.print(randomReading);
+           
+            
+            
+
+            // The HTTP response ends with another blank line:
+            client.println();
+            // break out of the while loop:
+            break;
+          }
+          else {      // if you got a newline, then clear currentLine:
+            currentLine = "";
+          }
+        }
+        else if (c != '\r') {    // if you got anything else but a carriage return character,
+          currentLine += c;      // add it to the end of the currentLine
+        }
+
+        if (currentLine.endsWith("GET /H")) {
+          carrier.leds.clear();
+
+          carrier.leds.setBrightness(20);
+
+          //carrier.leds.setPixelColor(index, red, green, blue);
+          carrier.leds.setPixelColor(0,0,255,0);
+          carrier.leds.setPixelColor(1,0,255,0);
+          carrier.leds.setPixelColor(2,0,255,0);
+          carrier.leds.setPixelColor(3,0,255,0);
+          carrier.leds.setPixelColor(4,0,255,0);
+          carrier.leds.show();
+
+          carrier.display.fillScreen(ST77XX_WHITE);
+          carrier.display.setTextColor(ST77XX_GREEN);
+          carrier.display.setTextSize(2);
+
+          carrier.display.setCursor(75, 110);
+          carrier.display.println("acceso");       
+        }
+        if (currentLine.endsWith("GET /L")) {
+          carrier.leds.clear();  
+          carrier.leds.show(); 
+
+          carrier.display.fillScreen(ST77XX_BLACK);
+          carrier.display.setTextColor(ST77XX_WHITE);
+          carrier.display.setTextSize(2);
+
+          carrier.display.setCursor(75, 110);
+          carrier.display.println("spento");    
+        }
+
+      }
+    }
+    // close the connection:
+    client.stop();
+    Serial.println("client disconnected");
+  }
+}
